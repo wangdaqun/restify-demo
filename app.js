3246ee031b0fe416bcd9440d532a8c0812e77ed8
wangdaqun <772033496@qq.com>
2016-04-07 22:29:29 +0800

    commit

--- app.js
+++ app.js
@@ -3,13 +3,39 @@
  */
 var restify = require('restify');
 
-function respond(req, res, next) {
+var server = restify.createServer({
+    name: 'myapp',
+    version: '1.0.0'
+});
+server.use(restify.acceptParser(server.acceptable));
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
+
+//server.get('/echo/:name', function (req, res, next) {
+
+function response (req, res, next) {
+
+    // Console.log(req.queryParser()) ;
+
+
+    console.log(req.query);
+    console.log();
+    console.log();
+    res.send(req.query.name+" -- "+req.query.id);
+    // res.send(req.params);
+    return next();
+}
+
+
+function respond(req, res) {
     res.send('hello ' + req.params.name);
 }
 
-var server = restify.createServer();
 server.get('/hello/:name', respond);
 
-server.listen(8080, function() {
+
+server.get('/echo',response);
+
+server.listen(8810, function () {
     console.log('%s listening at %s', server.name, server.url);
 });
